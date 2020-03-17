9d6ea652b1df79f7cc3b0db371226870c2d1f4b1
vaicThor <victor.louette.pro@gmail.com>
2020-03-17 12:52:04 +0100

    Error dans la classe : Request/Query non terminée + table non créée + méthode de l'interface non réussi

--- src/main/java/com/spring/planner/repository/ItemRepository.java
+++ src/main/java/com/spring/planner/repository/ItemRepository.java
@@ -1,8 +1,16 @@
 package com.spring.planner.repository;
 
+import com.spring.planner.entities.Unavailability;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepository<T> extends CrudRepository<T,Long> {
+
+    @Query("select c from unavailability_classroom_timeslot c WHERE c.classroom_name=:param")
+    Iterable<Unavailability> findAllByClassroomName(@Param("param") String classroomName);
 
 
 }
