f23f4798a8f395550fee7ba5e9ab47c73b0f64e7
vaicThor <victor.louette.pro@gmail.com>
2020-03-18 09:45:32 +0100

    classroom Service fait + classroom entity

--- src/main/java/com/spring/planner/entities/Classroom.java
+++ src/main/java/com/spring/planner/entities/Classroom.java
@@ -1,11 +1,19 @@
 package com.spring.planner.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="classroom")
 public class Classroom extends Item {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+
+    public Classroom() {
+    }
+
     public Classroom(Long id, String name) {
     super.id = id;
     super.name = name;
--- src/main/java/com/spring/planner/service/ClassroomService.java
+++ src/main/java/com/spring/planner/service/ClassroomService.java
@@ -1,7 +1,18 @@
 package com.spring.planner.service;
 
+import com.spring.planner.repository.ClassroomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClassroomService {
+
+    @Autowired
+    ClassroomRepository classroomRepository;
+
+public List fetchClassroomUnavailibility(Long id){
+    return (List) classroomRepository.findUnavailabilityByClassroomId(id);
+}
 }
