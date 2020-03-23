18c06796741b5f159b80a0113b545a53953a6d65
Aszel83 <a_k_i_m_@hotmail.fr>
2020-03-23 09:15:56 +0100

    pom.xml add dependency test

--- src/main/java/com/spring/planner/restcontroller/UnavailabilityRestController.java
+++ src/main/java/com/spring/planner/restcontroller/UnavailabilityRestController.java
@@ -31,7 +31,6 @@ public class UnavailabilityRestController {
     @RequestMapping("/deleteunavailability/{id}")
     public void deleteUnavailabilityById(@PathVariable("id")Long id){
         unavailabilityService.deleteUnavailabilityById(id);
-
     }
 
     /**
