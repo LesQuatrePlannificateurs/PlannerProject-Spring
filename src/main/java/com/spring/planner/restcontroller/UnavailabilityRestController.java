19f7219fb1972e9e8a50e66476e9a6fa84ffa7b2
Audenb <audenerozzibanfi@gmail.com>
2020-03-24 09:14:34 +0100

    url unavailability rest controller modifié

--- src/main/java/com/spring/planner/restcontroller/UnavailabilityRestController.java
+++ src/main/java/com/spring/planner/restcontroller/UnavailabilityRestController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
-//@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/apiUnavailability")
 public class UnavailabilityRestController {
 
     @Autowired
