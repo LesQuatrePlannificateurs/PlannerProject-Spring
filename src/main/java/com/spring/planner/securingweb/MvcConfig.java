6c4872d60d4d72cfe47af333b84d62a53c12c329
vaicThor <victor.louette.pro@gmail.com>
2020-03-26 18:15:54 +0100

    ajout de configurations sur POM.xml : Fonctionnel!

---
+++ src/main/java/com/spring/planner/securingweb/MvcConfig.java
@@ -0,0 +1,8 @@
+package com.spring.planner.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+}
