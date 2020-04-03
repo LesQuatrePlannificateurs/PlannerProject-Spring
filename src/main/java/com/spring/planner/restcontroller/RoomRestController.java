bed0853afdefd9a7abcb70206364f03c69a69fc0
Audenb <audenerozzibanfi@gmail.com>
2020-04-03 22:57:31 +0200

    retrait majuscule dans crossorigin room rest controller

--- src/main/java/com/spring/planner/restcontroller/RoomRestController.java
+++ src/main/java/com/spring/planner/restcontroller/RoomRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/apiRoom")
+@RequestMapping("/apiroom")
 public class RoomRestController {
 
 
